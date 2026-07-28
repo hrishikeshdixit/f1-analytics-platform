e8324425924e1d313c17488d6d772f6fb235e3c1
Hrishikesh Dixit <69136012+hrishikeshdixit@users.noreply.github.com>
2026-07-27 21:16:45 -0500

    Refactor imports in home.py
    Comment out unused imports related to track map components.

--- dashboards/views/home.py
+++ dashboards/views/home.py
@@ -4,7 +4,7 @@ from dashboards.utils.bigquery import get_races, get_drivers, get_max_laps
 from dashboards.utils.telemetry import get_telemetry, get_lap_telemetry, get_pit_exit
 from dashboards.utils.styles import get_color, set_background_theme, get_driver_photo, get_driver_line_style
 from dashboards.components.driver_cards import driver_card
-from dashboards.components.track_map import build_track_map, build_animation_frame, build_sector_track_map, build_3d_sector_track_map
+#from dashboards.components.track_map import build_track_map, build_animation_frame, build_sector_track_map, build_3d_sector_track_map
 
 
 def show():
